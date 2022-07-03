584520d8ebe68668329041986e27e3935d154103
Don Cassidy <sotosoc@protonmail.com>
2022-07-03 15:42:30 -0600

    zsh: setup export.zsh

---
+++ export.zsh
@@ -0,0 +1,18 @@
+#---------------------------------------------------------------------
+# Purpose: Set default EDITOR and VISUAL editor
+#---------------------------------------------------------------------
+export EDITOR=/usr/bin/vim
+export VISUAL=/usr/bin/gvim
+
+#---------------------------------------------------------------------
+# Purpose: Set MANPATH and INFOPATH
+#---------------------------------------------------------------------
+export MANPATH=$MANPATH:/usr/local/texlive2016/texmf-dist/doc/man
+export MANPATH=$MANPATH:/cos/home3/donc/me/repos/asic_design/flow/man
+export INFOPATH=$INFOPATH:/usr/local/texlive2016/texmf-dist/doc/info
+
+#---------------------------------------------------------------------
+# Purpose: Point to OH-MY-ZSH
+#---------------------------------------------------------------------
+export ZSH=$HOME/.oh-my-zsh
+
