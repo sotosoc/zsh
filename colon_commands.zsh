fc1e700f0ac6f0ec6a5d7f1b8dd11305f0c0fb8b
Don Cassidy <sotosoc@protonmail.com>
2022-07-03 15:34:03 -0600

    zsh: setup colon_commands

--- colon_commands.zsh
+++ colon_commands.zsh
@@ -1,12 +1,12 @@
 #-------------------------------------------------------------------------------
 # PURPOSE: colon commands are cd commands to working areas
 #-------------------------------------------------------------------------------
-alias  :bible='cd ~/repos/web_bible_ventilated ; source ~/repos/web_bible_ventilated/aliases.sh  ; clear; tree -L 1'
-alias   :drop='cd ~/Dropbox                                                                      ; clear; tree -L 1'
-alias  :novel='cd ~/repos/forestgate           ; source aliases.sh                               ; clear; tree -L 1'
-alias  :repos='cd ~/repos                                                                        ; clear; tree -L 1'
-alias   :sbsr='cd ~/repos/sbsr                 ; source ~/repos/sbsr/aliases.sh                  ; clear; tree -L 1'
-alias  :words='cd ~/repos/sbsr-words           ; source ~/repos/sbsr-words/aliases.sh            ; clear; tree -L 1'
-alias    :vim='cd ~/.vim                                                                         ; clear; tree -L 2'
-alias    :zsh='cd ~/repos/zsh                                                                    ; clear; tree -L 1'
+alias  :bible='cd ~/repos/web_bible_ventilated ; source ~/repos/web_bible_ventilated/aliases.sh  ; clear; pwd; tree -L 1'
+alias   :drop='cd ~/Dropbox                                                                      ; clear; pwd; tree -L 1'
+alias  :novel='cd ~/repos/forestgate           ; source aliases.sh                               ; clear; pwd; tree -L 1'
+alias  :repos='cd ~/repos                                                                        ; clear; pwd; tree -L 1'
+alias   :sbsr='cd ~/repos/sbsr                 ; source ~/repos/sbsr/aliases.sh                  ; clear; pwd; tree -L 1'
+alias  :words='cd ~/repos/sbsr-words           ; source ~/repos/sbsr-words/aliases.sh            ; clear; pwd; tree -L 1'
+alias    :vim='cd ~/.vim                                                                         ; clear; pwd; tree -L 2'
+alias    :zsh='cd ~/repos/zsh                                                                    ; clear; pwd; tree -L 1'
 
