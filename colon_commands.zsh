cbe420c1a6b802549b47a1e73725e430b87436e6
Don Cassidy <sotosoc@protonmail.com>
2022-07-03 15:27:59 -0600

    zsh: setup colon_commands

--- colon_commands.zsh
+++ colon_commands.zsh
@@ -1,11 +1,12 @@
 #-------------------------------------------------------------------------------
 # PURPOSE: colon commands are cd commands to working areas
 #-------------------------------------------------------------------------------
-alias  :bible='cd ~/repos/web_bible_ventilated ; . aliases.sh ; clear; ls -l'
-alias   :drop='cd ~/Dropbox                    ; clear; ls -l'
-alias  :novel='cd ~/repos/forestgate           ; . aliases.sh ; l'
-alias  :repos='cd ~/repos; tree -L 1'
-alias   :sbsr='cd ~/repos/sbsr                 ; . aliases.sh ; l'
-alias  :words='cd ~/repos/sbsr-words           ; . aliases.sh ; l'
-alias    :vim='cd ~/.vim; tree -L 2'
-alias    :zsh='cd ~/repos/zsh; tree -L 1'
+alias  :bible='cd ~/repos/web_bible_ventilated ; source ~/repos/web_bible_ventilated/aliases.sh  ; clear; tree -L 1'
+alias   :drop='cd ~/Dropbox                                                                      ; clear; tree -L 1'
+alias  :novel='cd ~/repos/forestgate           ; source aliases.sh                               ; clear; tree -L 1'
+alias  :repos='cd ~/repos                                                                        ; clear; tree -L 1'
+alias   :sbsr='cd ~/repos/sbsr                 ; source ~/repos/sbsr/aliases.sh                  ; clear; tree -L 1'
+alias  :words='cd ~/repos/sbsr-words           ; source ~/repos/sbsr-words/aliases.sh            ; clear; tree -L 1'
+alias    :vim='cd ~/.vim                                                                         ; clear; tree -L 2'
+alias    :zsh='cd ~/repos/zsh                                                                    ; clear; tree -L 1'
+
