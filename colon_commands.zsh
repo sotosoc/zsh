b1910cad037557be220e099bb7f41d011d9e9435
Don Cassidy <sotosoc@protonmail.com>
2022-07-03 15:18:17 -0600

    zsh: setup colon_commands

---
+++ colon_commands.zsh
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# PURPOSE: colon commands are cd commands to working areas
+#-------------------------------------------------------------------------------
+alias  :bible='cd ~/repos/web_bible_ventilated ; . aliases.sh ; clear; ls -l'
+alias   :drop='cd ~/Dropbox                    ; clear; ls -l'
+alias  :novel='cd ~/repos/forestgate           ; . aliases.sh ; l'
+alias  :repos='cd ~/repos; tree -L 1'
+alias   :sbsr='cd ~/repos/sbsr                 ; . aliases.sh ; l'
+alias  :words='cd ~/repos/sbsr-words           ; . aliases.sh ; l'
+alias    :vim='cd ~/.vim; tree -L 2'
+alias    :zsh='cd ~/repos/zsh; tree -L 1'
